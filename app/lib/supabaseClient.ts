5e81161a7ee9e918b793781d127e615c4c73bd2e
jikendakazu <lawson.singa@gmail.com>
2025-09-27 15:57:54 +0900

    Update supabaseClient.ts

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -2,6 +2,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,      // Vercelに入れた値を参照
+  process.env.NEXT_PUBLIC_SUPABASE_URL!, 
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
