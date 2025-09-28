4787827540e547eca86ec9c6dcff4ead4bf26a18
jikendakazu <lawson.singa@gmail.com>
2025-09-28 10:30:10 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // 余計な設定は入れない（特に output: 'export' を置かない）
+};
+
 export default nextConfig;
